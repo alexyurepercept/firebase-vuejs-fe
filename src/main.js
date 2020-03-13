a801c8494bec97633ea4fe2ce57572692ce88fd0
Alex Yu <alex.yu@repercept.com>
2020-03-14 09:10:14 +1100

    read credentials from environment variable

--- src/main.js
+++ src/main.js
@@ -23,7 +23,15 @@ Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 // Your web app's Firebase configuration
-var firebaseConfig = {}
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_API_KEY,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -45,6 +53,7 @@ router.beforeEach(async (to, from, next) => {
   }
   next();
 });
+
 new Vue({
   router,
   store,
